63ae85f267c05ece4be4b6104f9bd5ad7ab0cb8d
mahdi_ali <pakpak1251810@gmail.com>
2024-07-31 19:33:33 +0330

    add: footer,header, and home page codes and register the routes

---
+++ app/Livewire/AuctionsPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AuctionsPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.auctions-page');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\TestController;
+use App\Livewire\AuctionsPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/test/attenders', [TestController::class, 'getAttenders']);
+Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/auctions', AuctionsPage::class);
