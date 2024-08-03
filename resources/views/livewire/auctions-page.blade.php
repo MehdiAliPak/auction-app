61294eeac5887b2f282107f24c2eb83f1b5ff53b
mahdi_ali <pakpak1251810@gmail.com>
2024-08-03 14:53:08 +0330

    add: all the pages and register the routes

--- resources/views/livewire/auctions-page.blade.php
+++ resources/views/livewire/auctions-page.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
-    <section class="py-10 bg-gray-50 font-poppins dark:bg-gray-800 rounded-lg">
+    <section class="py-10 rounded-lg bg-gray-50 font-poppins dark:bg-gray-800">
       <div class="px-4 py-4 mx-auto max-w-7xl lg:py-6 md:px-6">
         <div class="flex flex-wrap mb-24 -mx-3">
           <div class="w-full pr-2 lg:w-1/4 lg:block">
@@ -65,7 +65,7 @@
               </ul>
             </div>
             <div class="p-4 mb-5 bg-white border border-gray-200 dark:bg-gray-900 dark:border-gray-900">
-              <h2 class="text-2xl font-bold dark:text-gray-400">Product Status</h2>
+              <h2 class="text-2xl font-bold dark:text-gray-400">auction Status</h2>
               <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
               <ul>
                 <li class="mb-4">
@@ -111,14 +111,14 @@
               <div class="w-full px-3 mb-6 sm:w-1/2 md:w-1/3">
                 <div class="border border-gray-300 dark:border-gray-700">
                   <div class="relative bg-gray-200">
-                    <a href="/products/product_one" class="">
+                    <a href="/auctions/auction_one" class="">
                       <img src="https://i.postimg.cc/hj6h6Vwv/pexels-artem-beliaikin-2292919.jpg" alt="" class="object-cover w-full h-56 mx-auto ">
                     </a>
                   </div>
                   <div class="p-3 ">
                     <div class="flex items-center justify-between gap-2 mb-2">
                       <h3 class="text-xl font-medium dark:text-gray-400">
-                        Product name
+                        auction name
                       </h3>
                     </div>
                     <p class="text-lg ">
@@ -127,7 +127,7 @@
                   </div>
                   <div class="flex justify-center p-4 border-t border-gray-300 dark:border-gray-700">
   
-                    <a href="#" class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
+                    <a href="#" class="flex items-center space-x-2 text-gray-500 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-cart3 " viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                       </svg><span>Add to Cart</span>
@@ -146,7 +146,7 @@
                   <div class="p-3 ">
                     <div class="flex items-center justify-between gap-2 mb-2">
                       <h3 class="text-xl font-medium dark:text-gray-400">
-                        Product name
+                        auction name
                       </h3>
                     </div>
                     <p class="text-lg ">
@@ -155,7 +155,7 @@
                   </div>
                   <div class="flex justify-center p-4 border-t border-gray-300 dark:border-gray-700">
   
-                    <a href="#" class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
+                    <a href="#" class="flex items-center space-x-2 text-gray-500 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-cart3 " viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                       </svg><span>Add to Cart</span>
@@ -174,7 +174,7 @@
                   <div class="p-3 ">
                     <div class="flex items-center justify-between gap-2 mb-2">
                       <h3 class="text-xl font-medium dark:text-gray-400">
-                        Product name
+                        auction name
                       </h3>
                     </div>
                     <p class="text-lg ">
@@ -183,7 +183,7 @@
                   </div>
                   <div class="flex justify-center p-4 border-t border-gray-300 dark:border-gray-700">
   
-                    <a href="#" class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
+                    <a href="#" class="flex items-center space-x-2 text-gray-500 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-cart3 " viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                       </svg><span>Add to Cart</span>
@@ -202,7 +202,7 @@
                   <div class="p-3 ">
                     <div class="flex items-center justify-between gap-2 mb-2">
                       <h3 class="text-xl font-medium dark:text-gray-400">
-                        Product name
+                        auction name
                       </h3>
                     </div>
                     <p class="text-lg ">
@@ -211,7 +211,7 @@
                   </div>
                   <div class="flex justify-center p-4 border-t border-gray-300 dark:border-gray-700">
   
-                    <a href="#" class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
+                    <a href="#" class="flex items-center space-x-2 text-gray-500 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-cart3 " viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                       </svg><span>Add to Cart</span>
@@ -230,7 +230,7 @@
                   <div class="p-3 ">
                     <div class="flex items-center justify-between gap-2 mb-2">
                       <h3 class="text-xl font-medium dark:text-gray-400">
-                        Product name
+                        auction name
                       </h3>
                     </div>
                     <p class="text-lg ">
@@ -239,7 +239,7 @@
                   </div>
                   <div class="flex justify-center p-4 border-t border-gray-300 dark:border-gray-700">
   
-                    <a href="#" class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
+                    <a href="#" class="flex items-center space-x-2 text-gray-500 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-cart3 " viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                       </svg><span>Add to Cart</span>
@@ -258,7 +258,7 @@
                   <div class="p-3 ">
                     <div class="flex items-center justify-between gap-2 mb-2">
                       <h3 class="text-xl font-medium dark:text-gray-400">
-                        Product name
+                        auction name
                       </h3>
                     </div>
                     <p class="text-lg ">
@@ -267,7 +267,7 @@
                   </div>
                   <div class="flex justify-center p-4 border-t border-gray-300 dark:border-gray-700">
   
-                    <a href="#" class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
+                    <a href="#" class="flex items-center space-x-2 text-gray-500 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-cart3 " viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                       </svg><span>Add to Cart</span>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
 use App\Http\Controllers\TestController;
+use App\Livewire\AuctionDetailPage;
 use App\Livewire\AuctionsPage;
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/auctions', AuctionsPage::class);
+Route::get('/auctions/{auction}', AuctionDetailPage::class);
+
+Route::get('/login', LoginPage::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/forgot', ForgotPasswordPage::class);
+Route::get('/reset', ResetPasswordPage::class);
+
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
